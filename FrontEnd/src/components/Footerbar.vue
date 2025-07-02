78724540ffacbb8fb0641a9b4e4af34e8364f761
Sergio R . Dominguez <sergio.sistemas2010@gmail.com>
2025-07-02 19:58:46 -0400

    update

--- FrontEnd/src/components/Footerbar.vue
+++ FrontEnd/src/components/Footerbar.vue
@@ -2,7 +2,7 @@
     <v-footer padless app inset>
         <v-card class="px-4 py-2 bg-blue-darken-4 text-center w-100">
             <v-card-text class="py-2 white--text text-center">
-                <strong>{{ new Date().getFullYear() }}</strong> - <strong>IT SOLUTIONS S.R.L.</strong>
+                <strong>{{ new Date().getFullYear() }}</strong> - <strong>IT SOLUTIONS</strong>
             </v-card-text>
         </v-card>
     </v-footer>
