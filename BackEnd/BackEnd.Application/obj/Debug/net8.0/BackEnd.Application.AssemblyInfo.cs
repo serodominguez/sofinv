36177852e4dc8555f16cb39f39adc841188da1b9
Sergio R . Dominguez <sergio.sistemas2010@gmail.com>
2025-07-02 19:04:21 -0400

    download project

--- BackEnd/BackEnd.Application/obj/Debug/net8.0/BackEnd.Application.AssemblyInfo.cs
+++ BackEnd/BackEnd.Application/obj/Debug/net8.0/BackEnd.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BackEnd.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b16a26b9e1bfe4948a8d440c7b63dee55054c92")]
 [assembly: System.Reflection.AssemblyProductAttribute("BackEnd.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BackEnd.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
